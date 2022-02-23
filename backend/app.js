51348a6233d6fd1e8e581626e02299dd004e5833
Daniel Biryak <gb802002@gmail.com>
2022-02-23 15:47:07 +0200

    Added files to work with GraphQL

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
 const server = require('express')()
-
+const {graphqlHTTP} = require('express-graphql')
+const cors = require('cors')
+const schema = require('./schema')
+server.use(cors())
+server.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema
+}))
 const PORT = '3000'
 
 server.get('/', (req, res) => {
     let response_text = 'Chillout  fdfsdsf'
 
-
-
     res.json({some_text: response_text})
 })
+
 
 
 server.listen(PORT)
